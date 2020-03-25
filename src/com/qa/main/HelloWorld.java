013af6196ada03bf0d99b0a5ec00dfe55acbfd0d
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-03-25 10:27:53 +0000

    first commit

---
+++ src/com/qa/main/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.qa.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		String hi = "Hello World";
+		System.out.println(hi);
+		
+		System.out.println(writeSomething());
+		
+	}
+	
+	public static String writeSomething() {
+		return "Hello World";
+		
+			}
+}
